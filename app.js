27824b27681f3be104308014d85859e9f493eb56
Govind Kumar <govindkumar3006@gmail.com>
2020-09-15 16:38:41 +0530

    Addaded edit and update feature

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var bodyParser = require("body-parser"),
+methodOverride = require("method-override"),
 mongoose       = require("mongoose"),
 express        = require("express"),
 app= express();
@@ -6,12 +7,14 @@ app= express();
 //MONGO setup
 mongoose.connect('mongodb://localhost:27017/restful_blog_app', {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useFindAndModify : false
 })  
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(methodOverride("_method"));
 
 //model config
 var blogSchema= new mongoose.Schema({
@@ -61,6 +64,28 @@ app.get("/blogs/:id", function(req, res){
             res.redirect("/blog");
         }else{
             res.render("show", {blog: foundBlog})
+        }
+    })
+})
+
+//edit route
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else{
+            res.render("edit", {blog: foundBlog});
+        }
+    })
+})
+
+//update route
+app.put("/blogs/:id", function(req, res){
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if(err){
+            res.redirect("/blogs");
+        }else{
+            res.redirect("/blogs/"+ req.params.id);
         }
     })
 })
